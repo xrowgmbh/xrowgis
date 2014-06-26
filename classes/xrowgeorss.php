13780c909c772b179313af376c4ef2f4daf8a27c
xinyuexrow <wang@xrow.de>
2014-06-26 15:19:52 +0200

    POI Bug fixed

--- classes/xrowgeorss.php
+++ classes/xrowgeorss.php
@@ -36,6 +36,8 @@ class xrowGEORSS
 
             foreach ( $treeNodes as $node )
             {
+                $poi_class=array("article","folder","event","location","organisation","contact");
+                if(! in_array($node->ClassIdentifier(),$poi_class))continue;
                 $collectionAttributes = array();
                 $dm = $node->dataMap();
                 if(empty($dm[$this->cache['cache'][$node->classIdentifier()]['gis']]))continue;
