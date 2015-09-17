fbcdfd6a76847996944fd02f1be018b041f18c28
xrowadmin <service@xrow.de>
2015-09-17 14:40:46 +0200

    Update ezjscore.ini.append.php

--- settings/ezjscore.ini.append.php
+++ settings/ezjscore.ini.append.php
@@ -6,11 +6,4 @@ FunctionList[]=xrowGIS_page
 [ezjscServer_xrowGIS_page]
 Class=xrowGISServerfunctions
 
-[eZJSCore]
-# Disables/enables js / css packer (for debugging Apache rewrite rules)
-# Normally controlled by [TemplateSettings]DevelopmentMode for convenience,
-# but can also be specifically controlled by this setting if set.
-# Force packer level by setting integer from 0 to 3 instead of [dis|en]abled
-Packer=disabled
-
 */ ?>
