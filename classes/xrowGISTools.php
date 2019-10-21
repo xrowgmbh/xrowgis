4542c78459d535f08c893f5045955df502dffce9
xinyuexrow <wang@xrow.de>
2019-10-21 18:22:48 +0200

    Mysql-Query Performance continue to optimize

--- classes/xrowGISTools.php
+++ classes/xrowGISTools.php
@@ -14,7 +14,7 @@ class xrowGISTools
 
         $object_state = ($params['object_state']!=null) ? true : false;
         
-        $subselect = ($object_state)?"JOIN ezcobj_state_link AS ezlink ON ezlink.contentobject_id = ezobj.id AND ezlink.contentobject_state_id = {$params['object_state']}" : "";
+        $subselect = ($object_state)?"JOIN ezcobj_state_link AS ezlink ON ezlink.contentobject_id = x.obj_id AND ezlink.contentobject_state_id = {$params['object_state']}" : "";
 
         $cacheDir = eZSys::cacheDirectory();
         $currentSiteAccessName = $GLOBALS['eZCurrentAccess']['name'];
@@ -27,25 +27,21 @@ class xrowGISTools
         $cacheFile = eZClusterFileHandler::instance( $cacheFilePath );
         if ( !$cacheFile->exists() or ( time() - $cacheFile->mtime() > $cacheTime ) )
         {
-            $list = $db->arrayQuery( "SELECT city, count(city) as 'count' FROM ezxgis_position AS gisp
-                                          JOIN ezcontentobject_attribute AS ezatt
-                                              ON gisp.contentobject_attribute_id = ezatt.id
-                                          JOIN ezcontentobject as ezobj
-                                              ON ezatt.contentobject_id = ezobj.id
-                                              AND ezobj.current_version = gisp.contentobject_attribute_version
-                                          JOIN ezcontentobject_tree as eztree
-                                              ON ezobj.id = eztree.contentobject_id
-                                              AND eztree.path_string LIKE '{$node->attribute('path_string')}%'
-                                              AND eztree.is_hidden = 0
-                                              AND eztree.is_invisible = 0
-                                          {$subselect}
-                                       WHERE city != ''
-                                       GROUP BY city
-                                       ORDER BY count desc", array('column' => 'city') );
+            $list = $db->arrayQuery( "(SELECT city, count(city) as 'count' FROM ezxgis_position AS gisp,
+                                          (SELECT ezatt.id AS obj_att_id, x.obj_id, x.obj_current_version FROM ezcontentobject_attribute AS ezatt,
+                                              (SELECT ezobj.id AS obj_id, ezobj.current_version AS obj_current_version FROM ezcontentobject AS ezobj,
+                                                  (SELECT contentobject_id FROM ezcontentobject_tree AS eztree
+                                                       WHERE eztree.path_string LIKE '{$node->attribute('path_string')}%' AND eztree.is_hidden = 0 AND eztree.is_invisible = 0
+                                                  ) AS z
+                                               WHERE z.contentobject_id = ezobj.id
+                                              ) AS x
+                                              {$subselect}
+                                           WHERE ezatt.contentobject_id = x.obj_id
+                                          ) AS y
+                                       WHERE city != '' AND gisp.contentobject_attribute_id = y.obj_att_id AND gisp.contentobject_attribute_version = y.obj_current_version
+                                       GROUP BY city ORDER BY null) ORDER BY count desc", array('column' => 'city') );
             $cacheFile->storeContents( serialize( $list ), 'query_cache', 'json' );
-        }
-        else
-        {
+        } else {
             $list = unserialize( $cacheFile->fetchContents() );
         }
         return $list;
